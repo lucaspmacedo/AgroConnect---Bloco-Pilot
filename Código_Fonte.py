8d07d8417e3ff9ddea41e1d68fc01d89da751f71
Lucas Macedo <lucas.macedo@aluno.unievangelica.edu.br>
2026-05-18 14:23:22 -0300

    3 FASE do Projeto Integrador

---
+++ Código_Fonte.py
@@ -0,0 +1,28 @@
+# ==========================================
+# PROJETO: AgroConnect - Bloco Pilot
+# FASE: 3 - Desenvolvimento de Protótipo
+# DESENVOLVEDOR: Lucas Macedo
+# DATA: 18/05/2026
+# ==========================================
+
+
+def menu_principal():
+    """Função que exibirá as opções do Bloco Pilot"""
+    print("-" * 30)
+    print("   AGROCONNECT - BLOCO PILOT   ")
+    print("-" * 30)
+    print("1. Painel Principal")
+    print("2. Cargas")
+    print("3. Financeiro")
+    print("0. Suporte")
+    print("-" * 30)
+
+
+def main():
+    # Aqui é onde o programa começa
+    menu_principal()
+    print("\n[Aviso] Lógica de processamento em desenvolvimento...")
+
+
+if __name__ == "__main__":
+    main()
